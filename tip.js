db341f1b0c531dda2e9f63359fd5ceeddc284655
Mac <mac@Macs-MacBook-Pro.local>
2022-07-31 20:18:06 +0400

    tip buttons function complete

--- tip.js
+++ tip.js
@@ -9,6 +9,12 @@ let totalAmount = 0;
 
 
 let tipButton5 = document.getElementById("btn5");
+let tipButton10 = document.getElementById("btn10");
+let tipButton15 = document.getElementById("btn15");
+let tipButton25 = document.getElementById("btn25");
+let tipButton50 = document.getElementById("btn50");
+
+
 let billInput = document.getElementById("b1");
 let tipResult = document.querySelector(".dollar2");
 let totalResult = document.querySelector(".dollar3");
@@ -49,3 +55,60 @@ totalResult.innerHTML =  totalAmount;
     
 })
 
+
+tipButton10.addEventListener('click', function(){
+
+  billAmount = billInput.value;
+
+
+  totalTip = billAmount * tipAmoun10 / 100;
+
+
+  tipResult.innerHTML = totalTip;
+
+
+
+})
+
+tipButton15.addEventListener('click', function (){
+
+
+billAmount = billInput.value;
+
+totalTip = billAmount * tipAmount15 / 100;
+
+
+tipResult.innerHTML = totalTip;
+ 
+
+
+})
+
+tipButton25.addEventListener('click', function(){
+
+
+  billAmount = billInput.value;
+  totalTip = billAmount * tipAmount25 / 100;
+
+  tipResult.innerHTML = totalTip;
+
+
+})
+
+tipButton50.addEventListener('click', function(){
+
+
+  billAmount = billInput.value;
+  totalTip = billAmount * tipAmount50 / 100;
+
+  tipResult.innerHTML = totalTip;
+
+})
+
+
+
+//RESET ღილაკზე დასაყენებელია ლისენერი, რომლის კლიკზეც გამოიძახება ფუნქცია, რომელიც tipButton-ებს 
+//ლისენერებს რიმუვს გაუკეთებს.
+
+
+//totalAmount არ აჯამებს tip & bill amount-ებს
