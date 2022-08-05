99c8fb4d792faa4eadc9516c2f97be7993376594
Mac <mac@Macs-MacBook-Pro.local>
2022-08-05 13:58:20 +0200

    add reset button function in js

--- tip.js
+++ tip.js
@@ -1,15 +1,18 @@
 
-
-
 let billAmount =0;
-let tipAmount5 =5;
-let tipAmoun10 =10;
-let tipAmount15 =15;
-let tipAmount25 =25;
-let tipAmount50 =50;
 let totalAmount = 0;
+let totalTip = 0;
+let buttonPercent = 1;
 
-let numberOfPeople = 0;
+
+
+
+
+let numberOfPeople = 1;
+
+let costumButton = 0;
+
+let resetAll;
 
 
 let tipButton5 = document.getElementById("btn5");
@@ -17,6 +20,9 @@ let tipButton10 = document.getElementById("btn10");
 let tipButton15 = document.getElementById("btn15");
 let tipButton25 = document.getElementById("btn25");
 let tipButton50 = document.getElementById("btn50");
+
+let resetButton = document.getElementById("rst");
+
 
 
 
@@ -30,12 +36,42 @@ let btnArr = document.querySelectorAll(".btnall");
 
 let numberOfPeopleresult = document.getElementById("b2");
 
+let costumButtonInput = document.getElementById("cstm");
 
+
+
+"use strict"
+
+billInput.addEventListener("input", function(event){
+
+
+  billAmount = billInput.value;
+  
+  totalTip = billAmount * buttonPercent/ 100;
+  totalAmount = parseInt(billAmount) + totalTip;
+  
+  
+  
+    //  console.log(billAmount, totalTip);
+
+  // tipResult.innerHTML = "$" + totalTip;
+  // totalResult.innerHTML = "$" + totalAmount;
+
+  // if (costumButton > 0 && numberOfPeople > 0 && billAmount > 0){
+
+    tipResult.innerHTML ="$" + (totalTip / numberOfPeople).toFixed(2);
+    totalResult.innerHTML = "$" + (totalAmount / numberOfPeople).toFixed(2);
+
+// }
+
+})
 
 for (let i = 0; i < btnArr.length; i++) {
 
     console.log(btnArr[i]);
     btnArr[i].addEventListener('click', function(event){
+
+      buttonPercent = event.target.value;
 
       // console.log(btnArr[i].textContent.replace("%", ""));
 
@@ -48,7 +84,7 @@ for (let i = 0; i < btnArr.length; i++) {
       tipResult.innerHTML = "$" + totalTip;
       totalResult.innerHTML = "$" + totalAmount;
       
-      if (numberOfPeople > 1){
+      if (numberOfPeople > 0 && billAmount > 0){
 
         tipResult.innerHTML ="$" + (totalTip / numberOfPeople).toFixed(2);
         totalResult.innerHTML = "$" + (totalAmount / numberOfPeople).toFixed(2);
@@ -64,13 +100,18 @@ for (let i = 0; i < btnArr.length; i++) {
    
   }
 
-  numberOfPeopleresult.addEventListener('input', function(){
+  
+
+  numberOfPeopleresult.addEventListener('input', function(event){
 
     numberOfPeople = numberOfPeopleresult.value;
 
-  
+    totalTip = billAmount * buttonPercent/ 100;
+    totalAmount = parseInt(billAmount) + totalTip;
 
-      if (numberOfPeople > 1){
+      console.log(event.target.value);
+
+      if (numberOfPeople > 0 && billAmount > 0){
 
         tipResult.innerHTML = "$" + (totalTip / numberOfPeople).toFixed(2);
         totalResult.innerHTML = "$" + (totalAmount / numberOfPeople).toFixed(2);
@@ -82,6 +123,49 @@ for (let i = 0; i < btnArr.length; i++) {
     }
 
   )
+
+
+  costumButtonInput.addEventListener('input', function(event){
+  
+     let costumButton = costumButtonInput.value;
+
+     let costumTip = costumButton * billAmount/100;
+     let costumTotal = parseInt(billAmount) + costumTip;
+
+      // tipResult.innerHTML = "$" + costumTip;
+      // totalResult.innerHTML = "$" + costumTotal;
+
+     if (costumButton > 0 && numberOfPeople > 0 && billAmount > 0){
+
+      tipResult.innerHTML = "$" + (costumTip/numberOfPeople).toFixed(2);
+      totalResult.innerHTML = "$" + (costumTotal/numberOfPeople).toFixed(2);
+      
+      console.log(billAmount);
+      console.log((tipResult, totalResult).textContent.replace("$", ""));
+      
+     }
+
+
+
+
+  })
+
+  resetButton.addEventListener('click', function(event){
+
+  location.reload();
+  
+
+  
+  })
+
+
+
+
+
+  //resset button - clear all input and outputs data
+  //costum inputing - placeholder changing to value and adding eventlistener
+
+
 
 
 
